5b716712002c01e6ed9e85aec73b738d4372e22b
Rhea Roy <rhearoy@bu.edu>
2016-04-17 15:03:51 -0400

    Added files via upload

--- Julia_SVM.R
+++ Julia_SVM.R
@@ -1,12 +1,14 @@
 setwd("~/Desktop")
 
-maintrain = read.csv("resizedTrain.csv", as.is = T)
-#test = read.csv("resizedTest.csv", as.is =T)
-#maintrain = read.csv("train.csv",as.is = T)
-#test = read.csv("test.csv", as.is = T)
+main = read.csv("resizedTrain.csv", as.is = T)
+labels = read.csv("trainLabels.csv", as.is=T)
+maintrain = merge(main, labels, by = "ID")
+
+
+
 
 # Split training data into training and test
-maintrain = maintrain[,c(2:401)]
+#maintrain = maintrain[,c(2:101)]
 set.seed(1)
 train = sample(dim(maintrain)[1],dim(maintrain)[1]/2)
 s_train = maintrain[train,]
@@ -17,26 +19,42 @@ s_train_y <- s_train[,1]
 s_test_x <- s_test[,-1]
 s_test_y <- s_test[,1]
 
+s_train_true = s_train_x$Class
+s_train_true = factor(s_train_true)
+s_test_true = s_test_x$Class
+s_test_true = factor(s_test_true)
+
 library("e1071")
+
+s_train_x = s_train_x[,c(2:100)]
+s_test_x = s_test_x[,c(2:100)]
 
 # center and scale the data sets
 s_train_x = scale(s_train_x,center=T, scale=T)
-s_train_y = scale(s_train_y,center=T, scale=T)
+#s_train_y = scale(s_train_y,center=T, scale=T)
 s_test_x = scale(s_test_x,center=T, scale=T)
-s_test_y = scale(s_test_y,center=T, scale=T)
+#s_test_y = scale(s_test_y,center=T, scale=T)
+
 
 
 # linear support vector classifier with  cost of 10
-svm.linear <- svm(s_train_x,s_train_y, data = dat.train, kernel = "linear",cost=10)
-plot(svm.linear, s_train_x)
+svm.linear <- svm(s_train_x,s_train_true, kernel = "linear",cost=10)
+
 
 lpredict = predict(svm.linear, s_train_x)
-p1 = as.data.frame(cbind(lpredict, s_train_y))
-p1$diff = (p1$lpredict - p1$V2)
+p1 = as.data.frame(cbind(lpredict, s_train_true))
+p1$diff = (p1$lpredict - p1$s_train_true)
 
-s1 = subset(p1, p1$diff == 0)
-# The bad news is, this gives us no differences of 0 which leads us to have a 
-# 0% accuracy rate...
+names(p1) = c("prediction","truth","correct")
+
+#This gives us an 84% accuracy! Now let's apply it to the real test data
+
+test = read.csv("resizedTest.csv", as.is =T)
+test = test[,c(2:101)]
+test = scale(test, center=100, scale=T)
+predict.test = predict(svm.linear, test)
+
+
 
 #  polynomial kernel of degree 3.
 svm.poly = svm(s_train_x, s_train_y, kernel = "polynomial",degree=3, cost = 10)
@@ -46,9 +64,12 @@ ppredict = predict(svm.poly, s_train_x)
 p2 = as.data.frame(cbind(ppredict, s_train_y))
 p2$diff = (p2$ppredict - p2$V2)
 
+names(p2) = c("prediction","truth","correct")
 s2 = subset(p2, p2$diff == 0)
 
 # Again, 0% accuracy :(
+
+#chi squared kernel
 
 # radial kernel and a gamma of 1.
 svm.radial <- svm(s_train_x, s_train_y,kernel = "radial", gamma = 1, cost = 10)
@@ -57,11 +78,11 @@ plot(svm.radial, s_train_x)
 rpredict = predict(svm.radial, s_train_x)
 p3 = as.data.frame(cbind(rpredict, s_train_y))
 p3$diff = (p3$rpredict - p3$V2)
+names(p3) = c("prediction","truth","correct")
 
 s3 = subset(p3, p3$diff == 0)
 # The bad news is, this gives us no differences of 0 which leads us to have a 
 # 0% accuracy rate...
-
 
 
 
